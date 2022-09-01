cde80cb5b1578acecc129aa7fc999da17f997e02
gaunt0066 <gaunt0066@gmail.com>
2022-09-01 11:48:06 +0300

    исправление ошибок

--- myobject.js
+++ myobject.js
@@ -5,12 +5,12 @@ engine:{temp:90, fuel: 4},
 autoCondition: 10,
 
 starting: function () {
-    car.engine.temp+10
-    car.
-    car.gearbox++
-    car.speed+10
+    car.engine.temp+=10
+
+    car.gearbox+=1
+    car.speed+=10
     console.log("двигатель запущен,передача включена")
-    if(rpm>11&car.engine.temp>130&car.engine.fuel<=0){console.log('отключите двигатель,перегрев')}
+    if(car.rpm>11&car.engine.temp>130&car.engine.fuel<=0){console.log('отключите двигатель,перегрев')}
 
   },
 
@@ -18,26 +18,26 @@ starting: function () {
     car.engine.fuel--
 car.engine.temp+10
 if(car.engine.fuel<0||car.engine.temp>140){
-    car.speed=0
-
-    console.log('топливо кончилось')
-}
+ console.log('топливо кончилось или двигатель перегрет')
+}else{console.log('едем')}
   },
   checking: function(){
+
     this.autoCondition--
     car.engine.temp++
-  if(car.rpm>10){
+  if(car.rpm>10&car.engine.temp>130){
     
     console.log('высокие обороты! Немедленно заглушите во избежание перегрева!')
   }
+  else{console.log('показания двигателя в порядке')}
   
     },
     checkAuto: function(){
         if(car.engine.temp>130){console.log('двигатель перегрет и выведен из строя')}
-
+else{console.log('все в порядке')}
     },
     current: function(){
-console.log(`обороты ${car.rpm} температура ${car.engine.temp} топливо ${car.engine.fuel} состояние авто ${car.autoCondition} `)
+console.log(`передача ${car.gearbox} обороты ${car.rpm} температура ${car.engine.temp} топливо ${car.engine.fuel} состояние авто ${car.autoCondition} `)
 if(car.autoCondition<=0){console.log('авто сломалось')}    
 
 
@@ -49,6 +49,9 @@ driveFast(){
     car.autoCondition--
     if(car.engine.fuel<0&car.engine.temp>100&car.autoCondition<0){
         console.log('car is broken')
+    }
+    else{
+      console.log('turbo режим активирован')
     }
 
 
